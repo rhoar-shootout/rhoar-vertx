17254d4e16067a82718b8784cb7b13ea1f58fb0e
Deven Phillips <deven.phillips@redhat.com>
2018-01-05 10:49:03 -0500

    Initial import

---
+++ insult/src/main/java/com/redhat/labs/insult/MainVerticle.java
@@ -0,0 +1,22 @@
+package com.redhat.labs.insult;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.ext.web.api.contract.openapi3.OpenAPI3RouterFactory;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
+        OpenAPI3RouterFactory.createRouterFactoryFromFile(vertx, "src/main/resources/insult.yaml", ar -> {
+            if (ar.succeeded()) {
+                // Spec loaded with success
+                OpenAPI3RouterFactory routerFactory = ar.result();
+            } else {
+                // Something went wrong during router factory initialization
+                Throwable exception = ar.cause();
+            }
+        });
+
+    }
+}
