50d60f354e28585ef876b28b1cb4b426c311e0c6
Deven Phillips <deven.phillips@redhat.com>
2018-04-07 15:25:38 -0400

    ui/src/main/java/com/redhat/labs/ui/MainVerticle.java
    Add UI health check endpoint

--- ui/src/main/java/com/redhat/labs/ui/MainVerticle.java
+++ ui/src/main/java/com/redhat/labs/ui/MainVerticle.java
@@ -1,5 +1,6 @@
 package com.redhat.labs.ui;
 
+import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.config.ConfigRetriever;
 import io.vertx.config.ConfigRetrieverOptions;
 import io.vertx.config.ConfigStoreOptions;
@@ -12,6 +13,8 @@ import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.StaticHandler;
 
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+
 public class MainVerticle extends AbstractVerticle {
 
     private final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
@@ -23,6 +26,7 @@ public class MainVerticle extends AbstractVerticle {
         ConfigRetrieverOptions retrieverOptions = new ConfigRetrieverOptions();
         // Check to see if we are running on Kubernetes/OCP
         if (System.getenv().containsKey("KUBERNETES_NAMESPACE")) {
+            router.route("/api/v1/health").handler(this::healthCheck);
             router.route("/statics/js/settings.js").handler(this::getConfig);
             router.route().handler(StaticHandler.create("webroot").setIndexPage("index.html"));
 
@@ -58,5 +62,9 @@ public class MainVerticle extends AbstractVerticle {
     private void getConfig(RoutingContext ctx) {
         ctx.response().headers().add("Content-Type", "application/javascript");
         ctx.response().end(config().getString("settings.js"));
+    }
+
+    private void healthCheck(RoutingContext ctx) {
+        ctx.response().setStatusCode(OK.code()).setStatusMessage(OK.reasonPhrase()).end();
     }
 }
